595ae6c4df281d68f63f57441052513c401347c1
August Saint Freytag <me@augustfreytag.com>
2023-12-03 12:33:02 +0100

    Set linting rules to only warn for debugger stmt.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
 	parser: "@typescript-eslint/parser",
 	plugins: ["react-refresh"],
 	rules: {
+		"no-debugger": "warn",
 		"@typescript-eslint/no-unused-vars": ["warn"],
 		"react-refresh/only-export-components": ["warn", { allowConstantExport: true }]
 	}
