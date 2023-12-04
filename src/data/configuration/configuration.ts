8d9b4f19036d18ff7fffb585773090ad0517423f
August Saint Freytag <me@augustfreytag.com>
2023-12-04 15:44:32 +0100

    Introduce configuration module for processing

---
+++ src/data/configuration/configuration.ts
@@ -0,0 +1,26 @@
+export const configuration = {
+	encoding: "windows1252", // Currently ignored
+	modPath: "E:/Steam/steamapps/common/Stalker GAMMA MO2/mods/ZZZ- Custom UI Patch",
+	modUserInterfaceFilePatterns: [
+		"actor_menu",
+		"message_box",
+		"pda",
+		"talk",
+		"ui_inventory",
+		"ui_keybinding",
+		"ui_mcm",
+		"ui_mm_main",
+		"ui_mm_faction_select",
+		"ui_mm_load_dlg",
+		"ui_mm_save_dlg",
+		"ui_options",
+		"ui_workshop"
+	],
+	modUserInterfaceLooseElements: [],
+	modUserInterfaceFileEnding: "xml",
+	lineHeightByFont: {
+		graffiti22: 24,
+		letterica18: 20,
+		letterica16: 18
+	}
+}
